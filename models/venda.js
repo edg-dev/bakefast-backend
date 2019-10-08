a81267727e2abd8a0e52f750d016c7d309a3aacd
theoam4 <31208962+theoam4@users.noreply.github.com>
2019-10-08 19:52:43 -0300

    Adicionando tempoInicio

--- models/venda.js
+++ models/venda.js
@@ -6,6 +6,7 @@ const mongoose = restful.mongoose;
 const vendaSchema = new mongoose.Schema({
     status : {type: Boolean, required:true },
     tempoChegada : {type : Number, required:true },
+    tempoInicio : {type: Number, required:true },
     valorTotal : {type: Number, required:true},
     produtos: [{
         nome: {type:String, required:true},
