5aca943953437837503ed48468d13dcd4f0b0657
Wender Lucas Souza <wenderlucas.souza@hotmail.com>
2019-11-05 17:39:31 -0300

    Retirado a sessão (configurada no front end).

--- config/server.js
+++ config/server.js
@@ -3,7 +3,6 @@ const port = 4000;
 const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
-const sess = require('express-session');
 
 const server = express();
 
@@ -12,16 +11,6 @@ server.use(bodyParser.json());
 
 server.use(cors());
 
-server.use(sess({
-    secret: 'shhhhh',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        secure: false,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}));
-var session;
 server.listen(process.env.PORT || port, function () {
     console.log('API rodando...');
 });
--- services/clienteService.js
+++ services/clienteService.js
@@ -1,19 +1,12 @@
-//Aqui fica os métodos de acesso da API
 const cliente = require('../models/cliente');
-const server = require('../config/server');
 
 cliente.route('login', function(req, res) {
     var user = req.body.username;
     var senha = req.body.senha;
-    var session = req.session;
 
     cliente.findOne({"usuario" : {"username": user, "senha": senha}}).exec()
         .then(function(result) {
-            //Adiciona os dados na sessão
-            session.userid = result.id;
-
-            //Redireciona para a página
-            res.send(result)
+            res.send(result);
         })
         .catch(error => {
             console.log(error.response);
@@ -21,10 +14,8 @@ cliente.route('login', function(req, res) {
     
 });
 
-//creating rest API, add CRUD to mongo's schema
 cliente.methods(['get', 'post', 'put','delete']);
 
-//return methods updated
 cliente.updateOptions({new: true, runValidators: true});
 
 module.exports = cliente;
