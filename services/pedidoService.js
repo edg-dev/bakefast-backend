033d63fae5123d1ec93af3a7aa657019df1e0f52
Wender lucas <wenderlucas.souza@hotmail.com>
2019-11-12 08:20:40 -0300

    Inicio da visualização de pedidos

--- services/pedidoService.js
+++ services/pedidoService.js
@@ -1,6 +1,18 @@
 //Aqui fica os métodos de acesso da API
 const pedido = require('../models/pedido');
 
+pedido.route('getPedidos', function(req, res){
+    var idPadaria = req.body.idPadaria;
+
+    pedido.find({"idPadaria": idPadaria}).exec()
+    .then(result => {
+        res.send(result);
+    })
+    .catch(error => {
+        console.log(error.response);
+    });
+});
+
 //creating rest API, add CRUD to mongo's schema
 pedido.methods(['get', 'post', 'put','delete'])
 
