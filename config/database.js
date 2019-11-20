4bf9a1462dafd347a8997bfc42eae728daeef49f
Wender Lucas Souza <wenderlucas.souza@hotmail.com>
2019-11-19 22:32:39 -0300

    Finalização da API - Hashing de senhas.

--- config/database.js
+++ config/database.js
@@ -4,4 +4,8 @@ const mongoose = require('mongoose');
 
 module.exports = mongoose.connect(process.env.MONGO_CONNECTIONSTRING , {
     useNewUrlParser: true
+}).then(() => {
+    console.log('Conexão efetuada com sucesso!');
+}).catch(err => {
+    console.log('Erro na conexão: ' + err);
 });
